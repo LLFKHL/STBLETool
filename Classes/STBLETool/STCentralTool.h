cfbc6f2ab507fdcd5ea388f42a8bd22e1ebc2a2a
TangJR <tangjr.work@gmail.com>
2016-03-19 18:00:13 +0800

    add ota data

--- Classes/STBLETool/STCentralTool.h
+++ Classes/STBLETool/STCentralTool.h
@@ -8,21 +8,49 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreBluetooth/CoreBluetooth.h>
+@class STCentralTool;
+
+static NSString * const STCentralErrorConnectTimeOut = @"time out";
+static NSString * const STCentralErrorConnectOthers = @"other error";
+static NSString * const STCentralErrorConnectPowerOff = @"power off";
+static NSString * const STCentralErrorConnectAutoConnectFail = @"auto connect fail";
 
 @protocol STCentralToolDelegate <NSObject>
 
+@optional
+- (void)centralTool:(STCentralTool *)centralTool findPeripherals:(NSArray *)peripherals;
 
+- (void)centralTool:(STCentralTool *)centralTool connectFailure:(NSError *)error;
+- (void)centralTool:(STCentralTool *)centralTool connectSuccess:(CBPeripheral *)peripheral;
+- (void)centralTool:(STCentralTool *)centralTool disconnectPeripheral:(CBPeripheral *)peripheral;
+
+- (void)centralTool:(STCentralTool *)centralTool recievedData:(NSData *)data;
+- (void)centralTool:(STCentralTool *)centralTool writeFinishWithError:(NSError *)error;
+
+@end
+
+@protocol STCentralToolOTADelegate <NSObject>
+
+@optional
+- (void)centralTool:(STCentralTool *)centralTool otaWriteLength:(NSInteger)length;
+- (void)centralTool:(STCentralTool *)centralTool otaWriteFinishWithError:(NSError *)error;
 
 @end
 
 @interface STCentralTool : NSObject
+
+@property (weak, nonatomic) id<STCentralToolDelegate, STCentralToolOTADelegate> delegate;
+@property (assign, nonatomic) BOOL isConnected; ///< 当前是否是连接状态
+@property (strong, nonatomic, readonly) CBCharacteristic *writeCharacteristic; ///< 需要写入的 chaeacteristic，因为有可能不止一个需要写入，所以在写入数据时，需要外部处理要写入哪一个
 
 + (instancetype)shareInstence;
 
 - (void)startScan;
 - (void)stopScan;
 - (void)selectPeripheral:(CBPeripheral *)peripheral;
+- (void)disconnectWithPeripheral:(CBPeripheral *)peripheral;
 
-- (void)sendData:(NSData *)data;
+- (void)sendData:(NSData *)data toCharacteristic:(CBCharacteristic *)toCharacteristic;
+- (void)otaUpdateData:(NSData *)data toCharacteristic:(CBCharacteristic *)toCharacteristic;
 
 @end
--- STBLETool/CentralVC.m
+++ STBLETool/CentralVC.m
@@ -9,7 +9,7 @@
 #import "CentralVC.h"
 #import "STCentralTool.h"
 
-@interface CentralVC ()
+@interface CentralVC () <STCentralToolDelegate, STCentralToolOTADelegate>
 
 @property (strong, nonatomic) STCentralTool *tool;
 
@@ -21,10 +21,51 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.tool = [STCentralTool shareInstence];
+    self.tool.delegate = self;
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+#pragma mark - Button Tapped
+
+- (IBAction)startScanButtonTapped:(UIButton *)sender {
     [self.tool startScan];
+}
+
+- (IBAction)sendDataButtonTapped:(UIButton *)sender {
+    NSData *data = [NSData new];
+    [self.tool otaUpdateData:data toCharacteristic:self.tool.writeCharacteristic];
+}
+
+#pragma mark - STCentralToolDelegate
+
+- (void)centralTool:(STCentralTool *)centralTool findPeripherals:(NSArray *)peripherals {
+    NSLog(@"%@", peripherals);
+    [self.tool selectPeripheral:[peripherals firstObject]];
+}
+
+- (void)centralTool:(STCentralTool *)centralTool connectFailure:(NSError *)error {
+    NSLog(@"连接错误 ---- %@", error);
+}
+
+- (void)centralTool:(STCentralTool *)centralTool connectSuccess:(CBPeripheral *)peripheral {
+    NSLog(@"连接成功 ---- %@", peripheral);
+}
+
+- (void)centralTool:(STCentralTool *)centralTool disconnectPeripheral:(CBPeripheral *)peripheral {
+    NSLog(@"准备断开连接");
+}
+
+- (void)centralTool:(STCentralTool *)centralTool recievedData:(NSData *)data {
+    NSLog(@"收到数据 ---- %@", data);
+}
+
+#pragma mark - STCentralToolOTADelegate
+
+- (void)centralTool:(STCentralTool *)centralTool otaWriteFinishWithError:(NSError *)error {
+    NSLog(@"传输完成，有错吗  ----- %@", error);
+}
+
+- (void)centralTool:(STCentralTool *)centralTool otaWriteLength:(NSInteger)length {
+    NSLog(@"已经传了这么长了啊 ------  %ld", length);
 }
 
 @end
